98eea8fb577a06285f1977461e36e316739c7363
AbelJulcaDataCix <datacixcustomer@gmail.com>
2025-09-08 11:22:38 -0500

    add sawgger in the proyect

---
+++ src/main/java/com/dataprogramming/security/config/SwaggerConfig.java
@@ -0,0 +1,52 @@
+package com.dataprogramming.security.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.http.HttpHeaders;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "API BANK Security V1",
+                description = "Our app provides the manager of type token jwt",
+                termsOfService = "www.dataprogramming.com/terminos_y_condiciones",
+                version = "1.0.0",
+                contact = @Contact(
+                        name = "Abel Julca",
+                        url = "https://dataprogramming.com",
+                        email = "datacixcustomer@gmail.com"
+                ),
+                license = @License(
+                        name = "Standard Software Use License for dataprogramming",
+                        url = "www.dataprogramming.com/licence"
+                )
+        ),
+        servers = {
+                @Server(
+                        description = "DEV SERVER",
+                        url = "http://localhost:8073"
+                )
+        },
+        security = @SecurityRequirement(
+                name = "Security Token"
+        )
+)
+@SecurityScheme(
+        name = "Security Token",
+        description = "Access Token For My API",
+        type = SecuritySchemeType.HTTP,
+        paramName = HttpHeaders.AUTHORIZATION,
+        in = SecuritySchemeIn.HEADER,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class SwaggerConfig {
+
+}
